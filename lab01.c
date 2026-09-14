f9ad49e83b0c1072f6e47ba0f702aa0f87539766
SebasTutaya-star <josemaria.tutaya.27@unsch.edu.pe>
2026-09-14 17:15:12 -0500

    Feat(parte-b): implementar captura por consola de 5 procesos con scanf

--- lab01.c
+++ lab01.c
@@ -7,5 +7,17 @@ struct Proceso {
 };
 
 int main() {
+    struct Proceso procesos[5];
+    int n = 5;
+    printf("=== Registro de Procesos (Total: %d) ===\n", n);
+    for (int i = 0; i < n; i++) {
+        printf("\nProceso %d:\n", i + 1);
+        printf("PID: ");
+        scanf("%d", &procesos[i].pid);
+        printf("Nombre: ");
+        scanf("%19s", procesos[i].nombre);
+        printf("Prioridad (1-5): ");
+        scanf("%d", &procesos[i].prioridad);
+    }
     return 0;
 }
